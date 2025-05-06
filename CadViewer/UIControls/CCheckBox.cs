9bbc6f478d8e05f3defe179ef45689da36ae00ec
thuong.nv <mark.ngo@kohyoung.com>
2025-05-06 16:33:13 +0700

    #0032 : Update checkbox animations

--- CadViewer/UIControls/CCheckBox.cs
+++ CadViewer/UIControls/CCheckBox.cs
@@ -35,6 +35,54 @@ namespace CadViewer.UIControls
 			};
 		}
 
+		private void UpdateVisualState()
+		{
+			var state = "";
+
+			if(IsMouseOver && IsChecked is true)
+			{
+				state = "MouseOver_Checked";
+			}
+			else if (IsMouseOver && IsChecked is false)
+			{
+				state = "MouseOver_Unchecked";
+			}
+			else if (!IsMouseOver && IsChecked is true)
+			{
+				state = "Normal_Checked";
+			}
+			else if (!IsMouseOver && IsChecked is false)
+			{
+				state = "Normal_Unchecked";
+			}
+
+			VisualStateManager.GoToState(this, state, true);
+		}
+
+		protected override void OnMouseEnter(MouseEventArgs e)
+		{
+			base.OnMouseEnter(e);
+			UpdateVisualState();
+		}
+
+		protected override void OnMouseLeave(MouseEventArgs e)
+		{
+			base.OnMouseLeave(e);
+			UpdateVisualState();
+		}
+
+		protected override void OnChecked(RoutedEventArgs e)
+		{
+			base.OnChecked(e);
+			UpdateVisualState();
+		}
+
+		protected override void OnUnchecked(RoutedEventArgs e)
+		{
+			base.OnUnchecked(e);
+			UpdateVisualState();
+		}
+
 		public static readonly DependencyProperty CheckWidthProperty =
 		DependencyProperty.Register(nameof(CheckWidth), typeof(double), typeof(CButton), new PropertyMetadata(double.NaN));
 
