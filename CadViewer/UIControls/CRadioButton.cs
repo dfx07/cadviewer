4aef74bc06ca55e260589e675501f4b18d2c59b6
thuong.nv <mark.ngo@kohyoung.com>
2025-05-23 16:11:09 +0700

    #0064: Add RadioButton control

---
+++ CadViewer/UIControls/CRadioButton.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+using System.Windows;
+using System.Windows.Media.Animation;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+using System.Windows.Input;
+using CadViewer.Animations;
+using System.Diagnostics;
+using System.Drawing.Printing;
+using System.Windows.Threading;
+
+namespace CadViewer.UIControls
+{
+	public class CRadioButton : RadioButton
+	{
+		static CRadioButton()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(CRadioButton), new FrameworkPropertyMetadata(typeof(CRadioButton)));
+		}
+
+		public override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+
+			Loaded += (s, e) =>
+			{
+				var thumb = GetTemplateChild("PART_Thumb") as Thumb;
+				var toolTip = GetTemplateChild("PART_Tooltip") as Popup;
+				var valueTip = GetTemplateChild("TooltipValue") as Border;
+			};
+		}
+
+		public static readonly DependencyProperty TrackHeightProperty =
+		DependencyProperty.Register(nameof(TrackHeight), typeof(double), typeof(CRadioButton), new PropertyMetadata(5.0));
+
+		public double TrackHeight
+		{
+			get => (double)GetValue(TrackHeightProperty);
+			set => SetValue(TrackHeightProperty, value);
+		}
+
+		public static readonly DependencyProperty TrackRadiusProperty =
+		DependencyProperty.Register(nameof(TrackRadius), typeof(CornerRadius), typeof(CRadioButton), new PropertyMetadata(new CornerRadius(3)));
+
+		public CornerRadius TrackRadius
+		{
+			get => (CornerRadius)GetValue(TrackRadiusProperty);
+			set => SetValue(TrackRadiusProperty, value);
+		}
+
+		public static readonly DependencyProperty ShowRangeLabelProperty =
+		DependencyProperty.Register( nameof(ShowRangeLabel), typeof(bool), typeof(CRadioButton), new PropertyMetadata(true));
+
+		public bool ShowRangeLabel
+		{
+			get => (bool)GetValue(ShowRangeLabelProperty);
+			set => SetValue(ShowRangeLabelProperty, value);
+		}
+	}
+}
