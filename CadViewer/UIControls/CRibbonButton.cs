e8910fc804ac630e2015e5a9cb94f20781a630bd
thuong.nv <thuong.nv.mta@gmail.com>
2025-05-31 00:23:35 +0700

    #0074: Add Ribbon bar buttons

--- CadViewer/UIControls/CRibbonButton.cs
+++ CadViewer/UIControls/CRibbonButton.cs
@@ -11,15 +11,22 @@ using System.Windows.Shapes;
 using System.Windows.Controls;
 using System.Windows.Input;
 using CadViewer.Animations;
+using CadViewer.Dialogs;
+using CadViewer.Interfaces;
 
 namespace CadViewer.UIControls
 {
-	public class CRibbonButton : ToggleButton
+	public enum ERibbonButtonDropDownDirection
 	{
-		static CRibbonButton()
+		Bottom,
+		Right
+	}
+
+	public class CRibbonToggleButton : ToggleButton
+	{
+		static CRibbonToggleButton()
 		{
-			DefaultStyleKeyProperty.OverrideMetadata(typeof(CRibbonButton),
-				new FrameworkPropertyMetadata(typeof(CRibbonButton)));
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(CRibbonToggleButton), new FrameworkPropertyMetadata(typeof(CRibbonToggleButton)));
 		}
 
 		public override void OnApplyTemplate()
@@ -27,7 +34,81 @@ namespace CadViewer.UIControls
 			base.OnApplyTemplate();
 		}
 
-		// Only support normal button style
+		public static readonly DependencyProperty ImageSourceProperty =
+		DependencyProperty.Register(nameof(ImageSource), typeof(ImageSource), typeof(CRibbonToggleButton), new PropertyMetadata(null));
+
+		public ImageSource ImageSource
+		{
+			get => (ImageSource)GetValue(ImageSourceProperty);
+			set => SetValue(ImageSourceProperty, value);
+		}
+
+		public static readonly DependencyProperty ImageWidthProperty =
+		DependencyProperty.Register(nameof(ImageWidth), typeof(double), typeof(CRibbonToggleButton), new PropertyMetadata(double.NaN));
+
+		public double ImageWidth
+		{
+			get => (double)GetValue(ImageWidthProperty);
+			set => SetValue(ImageWidthProperty, value);
+		}
+
+		public static readonly DependencyProperty OnTextProperty =
+		DependencyProperty.Register(nameof(OnText), typeof(string), typeof(CRibbonToggleButton), new PropertyMetadata("On"));
+
+		public string OnText
+		{
+			get => (string)GetValue(OnTextProperty);
+			set => SetValue(OnTextProperty, value);
+		}
+
+		public static readonly DependencyProperty OffTextProperty =
+		DependencyProperty.Register(nameof(OffText), typeof(string), typeof(CRibbonToggleButton), new PropertyMetadata("Off"));
+
+		public string OffText
+		{
+			get => (string)GetValue(OffTextProperty);
+			set => SetValue(OffTextProperty, value);
+		}
+
+		public static readonly DependencyProperty IsOpenDropDownProperty =
+		DependencyProperty.Register(nameof(IsOpenDropDown), typeof(bool), typeof(CRibbonToggleButton), new PropertyMetadata(false));
+		public bool IsOpenDropDown
+		{
+			get => (bool)GetValue(IsOpenDropDownProperty);
+			set => SetValue(IsOpenDropDownProperty, value);
+		}
+
+		public static readonly DependencyProperty DropDownContentProperty =
+		DependencyProperty.Register(nameof(DropDownContent), typeof(object), typeof(CRibbonToggleButton));
+		public object DropDownContent
+		{
+			get => GetValue(DropDownContentProperty);
+			set => SetValue(DropDownContentProperty, value);
+		}
+
+		public static readonly DependencyProperty DropDownDirectionProperty =
+		DependencyProperty.Register(nameof(DropDownDirection), typeof(ERibbonButtonDropDownDirection), 
+		typeof(CRibbonToggleButton), new PropertyMetadata(ERibbonButtonDropDownDirection.Bottom));
+
+		public ERibbonButtonDropDownDirection DropDownDirection
+		{
+			get => (ERibbonButtonDropDownDirection)GetValue(DropDownDirectionProperty);
+			set => SetValue(DropDownDirectionProperty, value);
+		}
+	}
+
+	public class CRibbonButton : Button
+	{
+		static CRibbonButton()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(CRibbonButton), new FrameworkPropertyMetadata(typeof(CRibbonButton)));
+		}
+
+		public override void OnApplyTemplate()
+		{
+			base.OnApplyTemplate();
+		}
+
 		public static readonly DependencyProperty ImageSourceProperty =
 		DependencyProperty.Register(nameof(ImageSource), typeof(ImageSource), typeof(CRibbonButton), new PropertyMetadata(null));
 
@@ -78,6 +159,16 @@ namespace CadViewer.UIControls
 		{
 			get => GetValue(DropDownContentProperty);
 			set => SetValue(DropDownContentProperty, value);
+		}
+
+		public static readonly DependencyProperty DropDownDirectionProperty =
+		DependencyProperty.Register(nameof(DropDownDirection), typeof(ERibbonButtonDropDownDirection),
+		typeof(CRibbonButton), new PropertyMetadata(ERibbonButtonDropDownDirection.Bottom));
+
+		public ERibbonButtonDropDownDirection DropDownDirection
+		{
+			get => (ERibbonButtonDropDownDirection)GetValue(DropDownDirectionProperty);
+			set => SetValue(DropDownDirectionProperty, value);
 		}
 	}
 }
--- CadViewer/ViewModels/RibbonBarViewModel.cs
+++ CadViewer/ViewModels/RibbonBarViewModel.cs
@@ -10,6 +10,8 @@ namespace CadViewer.ViewModels
 {
 	public class RibbonBarViewModel : ViewModelBase
 	{
+		private readonly Dictionary<string, object> _ribbonContentTabs = new Dictionary<string, object>();
+
 		private ObservableCollection<TabBarItemInfo> _tabBarInfos;
 		public ObservableCollection<TabBarItemInfo> TabBarInfos
 		{
@@ -56,24 +58,44 @@ namespace CadViewer.ViewModels
 
 			SelectedTab = TabBarInfos.FirstOrDefault();
 		}
+		private void LoadRibbonContentForTab(TabBarItemInfo tab)
+		{
+			if (tab == null)
+				return;
+
+			object NewViewModel = null;
+
+			if (_ribbonContentTabs.ContainsKey(tab.strTabName))
+			{
+				if (_ribbonContentTabs.TryGetValue(tab.strTabName, out var curTab))
+				{
+					NewViewModel = curTab;
+				}
+			}
+			else
+			{
+				switch (tab.strTabName.ToUpper())
+				{
+					case "HOME":
+						NewViewModel = new HomeBarViewModel();
+						break;
+					case "EDIT":
+						NewViewModel = new EditBarViewModel();
+						break;
+					default:
+						break;
+				}
+
+				if (NewViewModel != null)
+					_ribbonContentTabs[tab.strTabName] = NewViewModel;
+			}
+
+			RibbonContent = NewViewModel;
+		}
 
 		private void OnTabChanged(TabBarItemInfo tabinfo)
 		{
-			ViewModelBase CurrentViewModel = null;
-
-			switch(tabinfo.strTabName.ToUpper())
-			{
-				case "HOME":
-					CurrentViewModel = new HomeBarViewModel();
-					break;
-				case "EDIT":
-					CurrentViewModel = new EditBarViewModel();
-					break;
-				default:
-					break;
-			}
-
-			RibbonContent = CurrentViewModel;
+			LoadRibbonContentForTab(tabinfo);
 		}
 	}
 }
